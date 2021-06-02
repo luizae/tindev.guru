0fbe26111e27dd194942b787417bca3fcfd601c3
Michel Wilhelm <michel.wilhelm@loadsmart.com>
2021-06-02 13:57:06 -0300

    Show the sentence and update it

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,25 @@
 module.exports = {
-    'env': {
-        'es6': true
+  env: {
+    es6: true,
+    browser: true
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
     },
-    'extends': [
-        'eslint:recommended',
-        'plugin:react/recommended'
-    ],
-    'globals': {
-        'Atomics': 'readonly',
-        'SharedArrayBuffer': 'readonly'
-    },
-    'parserOptions': {
-        'ecmaFeatures': {
-            'jsx': true
-        },
-        'ecmaVersion': 2018,
-        'sourceType': 'module'
-    },
-    'plugins': [
-        'react'
-    ],
-    'rules': {
-        'indent': [
-            'error',
-            4
-        ],
-        'linebreak-style': [
-            'error',
-            'unix'
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'always'
-        ]
-    }
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: ['react'],
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always']
+  }
 };
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
+
+import './styles.scss';
+import sentencesData from '../../data/sentences.json';
+
+const getRandomSentence = () => {
+  return sentencesData[Math.floor(Math.random() * sentencesData.length)];
+};
 
 const HomePage = props => {
-  const { ...rest } = props;
+  const [sentence, setSentence] = useState(getRandomSentence);
+
+  useMemo(() => {
+    document.addEventListener('keyup', event => {
+      if (event.key === ' ') {
+        setSentence(getRandomSentence());
+      }
+    });
+  }, []);
+
+  const handleCopySentence = () => {
+    const el = document.createElement('textarea');
+    el.value = sentence.text;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+  };
 
   return (
-    <div {...rest}>
-      hello world
+    <div className="home-page">
+      <h1 onClick={() => handleCopySentence()}>{sentence.text}</h1>
+      <p>
+        Pressione <span>SPACE</span> para alterar a qantada ou{' '}
+        <span
+          onClick={() => {
+            setSentence(getRandomSentence());
+          }}>
+          clique aqui
+        </span>
+        qantada
+      </p>
     </div>
   );
 };
