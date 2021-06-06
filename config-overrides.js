55d399e78ba8c16a9e3c54ab33b97a7b6767e5fc
Michel Wilhelm <michelwilhelm@gmail.com>
2021-06-06 07:19:55 +0000

    update 12

--- config-overrides.js
+++ config-overrides.js
@@ -2,11 +2,11 @@ const { override } = require('customize-cra');
 const cspHtmlWebpackPlugin = require("csp-html-webpack-plugin");
 
 const cspConfigPolicy = {
-  'default-src': "'none'",
+  'default-src': "'self'",
   'base-uri': "'self'",
   'object-src': "'none'",
   'script-src': ["'self'"],
-  'style-src': ["'self'"]
+  'style-src': ["'self'", "'unsafe-inline'"]
 };
 
 function addCspHtmlWebpackPlugin(config) {
